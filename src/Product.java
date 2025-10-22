8a5dedf858a2e604ac6328b562c9d6e9d7ba4f63
nandin-verma97 <nandiniv88158@gmail.com>
2025-10-22 11:03:14 +0530

    Added product class second time

--- src/Product.java
+++ src/Product.java
@@ -1,4 +1,32 @@
 
 public class Product {
-
+   private String productId;
+   private String productName;
+   private int threshold;
+   private int quantity;
+   public String getProductId() {
+	return productId;
+   }
+   public void setProductId(String productId) {
+	this.productId = productId;
+   }
+   public String getProductName() {
+	return productName;
+   }
+   public void setProductName(String productName) {
+	this.productName = productName;
+   }
+   public int getThreshold() {
+	return threshold;
+   }
+   public void setThreshold(int threshold) {
+	this.threshold = threshold;
+   }
+   public int getQuantity() {
+	return quantity;
+   }
+   public void setQuantity(int quantity) {
+	this.quantity = quantity;
+   }
+   
 }
