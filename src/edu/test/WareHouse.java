2f8c2802830a1027ef6d85dc0daf4e7cb77dd7e9
nandin-verma97 <nandiniv88158@gmail.com>
2025-10-23 09:12:45 +0530

    Added the recive method

--- src/edu/test/WareHouse.java
+++ src/edu/test/WareHouse.java
@@ -18,7 +18,13 @@ public class WareHouse {
 		l.add(new Product("P108", "LIGHTPEN", 20, 50));
 
 		int choice = 0;
+		ListIterator<Product> itr = null;
+		Product p = null;
+		String id = null, name = null;
+		int quantity = 0, threshold = 0;
 		Scanner sc = new Scanner(System.in);
+		
+
 		while (true) {
 			System.out.println("");
 			System.out.println("1.DISPLAY ITEMS");
@@ -34,9 +40,8 @@ public class WareHouse {
 			switch (choice) {
 			case 1:
 				// DISPLAY
+				itr = l.listIterator();
 
-				ListIterator<Product> itr = l.listIterator();
-				Product p = null;
 				while (itr.hasNext()) {
 					p = itr.next();
 					System.out.println(p);
@@ -44,36 +49,97 @@ public class WareHouse {
 				break;
 
 			case 2:
-	            System.out.println("ENTER ID OF ITEM YOU WANT TO SEARCH");
-	            String id = sc.next();
-	            ListIterator<Product> itr1 = l.listIterator();
-	            Product p1 = null;
-	            boolean found  = false;
-	            while(itr1.hasNext()){
-	            	    p1  = itr1.next();
-	                if(p1.getProductId().equals(id)){
-	                	   found = true;
-	                   System.out.println("Here is your Product:" + p1);
-	                   break;
-	                }
-	            }
-	            if(!found) {
-	            	System.out.println("Item not found for the Id:" + id);
-	            }
-	            break;
-	  
+				System.out.println("ENTER ID OF ITEM YOU WANT TO SEARCH");
+				id = sc.next();
+				itr = l.listIterator();
+				boolean found = false;
+
+				while (itr.hasNext()) {
+					p = itr.next();
+					if (p.getProductId().equals(id)) {
+						found = true;
+						System.out.println("Here is your Product:");
+						System.out.println(p);
+						break;
+					}
+				}
+				if (!found) {
+					System.out.println("Item not found for the Id:" + id);
+				}
+				break;
 
 			case 3:
 				// add new product
+				System.out.println("Enter Product Id");
+				id = sc.next();
+				System.out.println("Enter Product name");
+				name = sc.next();
+				System.out.println("Enter product quantity");
+				quantity = sc.nextInt();
+				sc.nextLine();
+				System.out.println("Enter product threshold");
+				threshold = sc.nextInt();
+				sc.nextLine();
+				boolean found1 = false;
+
+				itr = l.listIterator();
+				while (itr.hasNext()) {
+					p = itr.next();
+					if (p.getProductId().equals(id)) {
+						found1 = true;
+						int qty = p.getQuantity();
+						qty = qty + quantity;
+						p.setQuantity(qty);
+						System.out.println("Product already exist with Id: " + p.getProductId() + "and Name: " + p.getProductName());
+						break;
+					}
+
+				}
+				if (!found1) {
+					p = new Product(id, name, quantity, threshold);
+					l.add(p);
+					System.out.println("Product added successfully");
+				}
+				break;
 
 			case 4:
 				// receive shipment
+				
+				//either stock is finished or stocked limited the threshold
+				
+				
+				System.out.println("Enter the Product Id of the product you want to Order");
+				id = sc.next();
+				System.out.println("Enter the quantity of Product");
+				quantity = sc.nextInt();
+				sc.nextLine();
+				itr = l.listIterator();
+				boolean found2 = false;
+				while(itr.hasNext()) {
+					p = itr.next();
+					if(p.getProductId().equals(id)) {
+						found2 = true;
+						int qty = p.getQuantity();
+						qty = qty +  quantity;
+						p.setQuantity(qty);
+						System.out.println("SHIPMENT RECEIVED SUCCESSFULLY");
+						
+						if (p.getQuantity() > p.getThreshold()) {
+			                System.out.println("Stock is now above threshold level (" + p.getThreshold() + ")");
+			            }
+						break;
+					}
+				}
+				if(!found2) {
+					System.out.println("Product not found for the Id :" + id );
+				}
+				break;
 
 			case 5:
 				// fulfill order
 
 			case 6:
-				System.exit(1);
+				System.exit(0);
 
 			default:
 				System.out.println("Invalid choice");
